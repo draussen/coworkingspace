804b6743a6c91202d01d1d78a1b7661f224f8462
Drin.Saiti <drinsaiti@gmail.com>
2023-12-04 13:19:20 +0100

    added ApplicationUser model

---
+++ src/main/java/ch/zli/m223/model/ApplicationUser.java
@@ -0,0 +1,103 @@
+package ch.zli.m223.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "ApplicationUser.findByEmail", query = "SELECT u FROM ApplicationUser u WHERE u.email = :email")
+})
+public class ApplicationUser {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(readOnly = true)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String surname;
+
+  @ManyToOne
+  @JoinColumn(name = "role_id")
+  private Role role;
+
+  @OneToMany(mappedBy = "user")
+  private List<Booking> bookings;
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public List<Booking> getBookings() {
+    return bookings;
+}
+
+public void setBookings(List<Booking> bookings) {
+    this.bookings = bookings;
+}
+}
