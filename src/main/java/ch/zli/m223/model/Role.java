f33809006fdff8a875f0ad2db81ce5e9dd5aeb23
Drin.Saiti <drinsaiti@gmail.com>
2023-12-04 13:20:31 +0100

    added Role model

---
+++ src/main/java/ch/zli/m223/model/Role.java
@@ -0,0 +1,32 @@
+package ch.zli.m223.model;
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(readOnly = true)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
